d733a1253c3305c5589ee91667291553a1f1f4aa
Jonathan <websites@jonathanbuchner.com>
2016-08-29 12:18:56 -0700

    add display pet function
    Add div section .notAdpoted to display the pet to index.html.
    Add Pet method .shos to append pet information to .notAdopted.
    Add temp funcion practiceCat.show and temp var practiceCat for testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 
+
+
+
 //Pet object
 function Pet (type, breed, name, age, color) {
   this.typeOf = type;
@@ -8,3 +11,23 @@ function Pet (type, breed, name, age, color) {
   this.colorOf = color;
   this.adopted = false;
 }
+
+//Short Term Pet --- DELETE ME!
+var practiceCat = new Pet("cat", "Calico", "Tootsie", 4, ["white","black", "brown"])
+
+//This will display pet information
+Pet.prototype.show = function () {
+    $(".newPet").append(""+
+    '<h3>'+this.nameOf+'<small> ('+this.typeOf+')</small></h3>' +
+    '<div class="showMorePet">' +
+      '<ul>' +
+        '<li>'+this.breed+'</li>' +
+        '<li>'+this.ageOf+'</li>' +
+        '<li>'+this.colorOf+'</li>' +
+      '</ul>' +
+    '</div> ' +
+'');}
+
+$(function(){
+practiceCat.show();
+});
