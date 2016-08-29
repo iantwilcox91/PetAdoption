1ffaebbea20722df48ebd0bd5b3cf4d8b2bf06c8
Jonathan <websites@jonathanbuchner.com>
2016-08-29 11:37:10 -0700

    Add pet object

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+//Pet object
+function Pet (type, specie, name, age, color, attitude) {
+  this.typeOf = type;
+  this.specieOf = specie;
+  this.nameOf = name;
+  this.ageOf = age;
+  this.colorOf = color;
+  this.attitudeOf = attitude;
+  this.adopted = false;
+}
+
+  
