629b5236a12fbfa806bf97ec0e9528b12adb8f13
Ian Wilcox <ian@>
2016-08-29 12:28:51 -0700

    about to merge, added new pet

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 //Pet object
 function Pet (type, breed, name, age, color) {
   this.typeOf = type;
@@ -8,3 +7,51 @@ function Pet (type, breed, name, age, color) {
   this.colorOf = color;
   this.adopted = false;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function(){
+
+
+
+
+  $("#theDonateButton").click(function() {
+    var type = $('input[name=type]:checked', '.typeOfAnimal').val();
+    var breed = $("input#breedOfAnimal").val();
+    var name = $("input#nameOfAnimal").val();
+    var age = $("input#ageOfAnimal").val();
+    var color = $(":checkbox:checked").map(function() { return this.value; }).get().join().split(",");
+    var newPet = new Pet(type, breed, name, age, color);
+    console.log(newPet)
+  });
+
+
+
+
+});
